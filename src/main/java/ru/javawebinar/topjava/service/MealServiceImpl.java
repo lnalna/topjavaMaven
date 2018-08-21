29f5324dd80af75a9097f98f21091e27b8dad37a
nik <lnalna@yandex.ru>
2018-08-22 00:38:12 +0300

    add application layers

---
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.javawebinar.topjava.service;
+
+import ru.javawebinar.topjava.repository.MealRepository;
+
+public class MealServiceImpl implements MealService {
+    private MealRepository repository;
+}
