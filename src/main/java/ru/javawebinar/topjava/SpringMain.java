3e8bf52b4847700e0368f8615a6130d119855cc0
nik <lnalna@yandex.ru>
2018-09-01 20:40:28 +0300

    add annotation

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,21 +4,18 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
 public class SpringMain {
     public static void main(String[] args) {
-        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("Bean definition names : " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
+            System.out.println("Bean definition names : " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        UserRepository userRepository = appCtx.getBean(UserRepository.class);
-        userRepository.getAll();
-
-        UserService userService = appCtx.getBean(UserService.class);
-        userService.save(new User(null, "userName1", "email1", "password1", Role.ROLE_ADMIN));
-        appCtx.close();
+            AdminRestController adminRestController = appCtx.getBean(AdminRestController.class);
+            adminRestController.create(new User(null, "userNameAdmin", "email1", "password1", Role.ROLE_ADMIN));
+            appCtx.close();
+        }
     }
 }
