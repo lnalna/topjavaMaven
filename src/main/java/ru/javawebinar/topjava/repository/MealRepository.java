18f44c379a1975890ca1ee5d58abb07e517b0523
nik <lnalna@yandex.ru>
2018-09-02 16:41:36 +0300

    implements MealRepository with userId

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,14 +2,20 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
-import java.util.Collection;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MealRepository {
-    Meal save(Meal meal);
+    //null if meal do not belong to user
+    Meal save(Meal meal, int userId);
 
-    void delete(int id);
+    //false if meal do not belong to user
+    boolean delete(int id, int userId);
 
-    Meal get(int id);
+    //null if meal do not belong to user
+    Meal get(int id, int userId);
 
-    Collection<Meal> getAll();
+    List<Meal> getAll(int userId);
+
+    List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId);
 }
