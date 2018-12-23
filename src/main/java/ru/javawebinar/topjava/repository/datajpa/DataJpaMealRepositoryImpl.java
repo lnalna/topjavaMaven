892a9332833a4a58fe087e9d1d60efe785ce95a3
nik <lnalna@yandex.ru>
2018-12-23 17:23:34 +0300

    add @Autowired CrudMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
-
+    @Autowired
     private CrudMealRepository crudMealRepository;
     @Autowired
     private CrudUserRepository crudUserRepository;
